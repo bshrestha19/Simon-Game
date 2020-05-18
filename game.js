eec252891fa157417b94c5d541dbbc7d6de4dbaf
Bibhor Shrestha <bshrestha19@gmail.com>
2020-05-19 00:57:55 +0100

    add tap event for mobile

--- game.js
+++ game.js
@@ -14,6 +14,16 @@ $(document).keypress(function () {
     }
 });
 
+$(document).on("tap", function () {
+    if (!started) {
+        $("#level-title").text("Level " + level);
+        nextSequence();
+        started = true;
+    }
+});
+
+
+
 $(".btn").click(function () {
 
     var userChosenColour = $(this).attr("id");
